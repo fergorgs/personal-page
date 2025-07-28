36f12a8bb80fdcf8764d0ddd93484d0515f86cf3
Fernando Fayet <fergorgu@amazon.com>
2025-07-27 21:58:39 -0300

    fix: broken project links

--- src/screens/common/ExternalButton.js
+++ src/screens/common/ExternalButton.js
@@ -2,7 +2,7 @@
 function ExternalButton({ text, url }) {
 
     return (
-        <div class="external-button" onClick={() => {window.location.href = {url}; }}> {/*class="about-button long-button"*/}
+        <div class="external-button" onClick={() => {window.location.href = url; }}> {/*class="about-button long-button"*/}
             <h4>{text}</h4>
         </div>
     );
