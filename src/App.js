0e2604f56b94c720fb54525056529777f9a8eea7
Fernando Fayet <fergorgu@amazon.com>
2025-08-08 11:04:09 -0300

    feat: add custom favicon and page title

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './styles/App.css'
+import { useEffect } from 'react';
 import WelcomeScreen from './screens/welcome/WelcomeScreen';
 import WorkScreen from './screens/work/WorkScreen';
 import ProjectsScreen from './screens/projects/ProjectsScreen';
@@ -8,6 +9,19 @@ import NavBar from './screens/common/NavBar';
 import Footer from './screens/common/Footer';
 
 function App() {
+
+  useEffect(() => {
+    document.title = "Fernando Fayet";
+    const link = document.querySelector("link[rel~='icon']");
+    if (link) {
+      link.href = '/assets/favicon.ico';
+    } else {
+      const newLink = document.createElement('link');
+      newLink.rel = 'icon';
+      newLink.href = '/assets/favicon.ico';
+      document.head.appendChild(newLink);
+    }
+  }, []);
 
   return (
     <div>
